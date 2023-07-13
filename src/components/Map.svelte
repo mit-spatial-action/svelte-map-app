bce8cfa90a5ee9ac3d3856f12470609793c0ae5a
Eric Robsky Huntley <ehuntley@mit.edu>
2023-07-13 19:10:39 -0400

    relocate geocoder, capture coordinates of results

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -10,34 +10,49 @@
     });
 
     export let lat;
-    export let lon;
+    export let lng;
     export let init_zoom;
     export let final_zoom;
 
     let container;
     let map;
+    let marker;
 
     onMount(() => {
         map = new mapbox.Map({
             container: container,
             // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
             style: 'mapbox://styles/ericrobskyhuntley/cljrocy4m017701pa1j212ahy',
-            center: [lon - 10, lat - 10],
+            center: [lng - 10, lat - 10],
             zoom: init_zoom,
             bearing: 0,
             projection: 'globe' // starting projection
         });
+        const geocoder = new MapboxGeocoder({
+            accessToken: mapbox.accessToken,
+            mapboxgl: mapbox
+        });
+        document.getElementById('geocoder').appendChild(geocoder.onAdd(map));
+        geocoder.on('result', e => {
+            lat = e.result.geometry.coordinates[1]
+            lng = e.result.geometry.coordinates[0]
+        });
         map.on('style.load', () => {
-            map.addControl(
-                new MapboxGeocoder({
-                    accessToken: mapbox.accessToken,
-                    mapboxgl: mapbox
-                })
-            );
             map.on('click', (e) => {
-                new mapbox.Marker()
+                if(typeof marker !== 'undefined') {
+                    marker.remove();
+                }
+                marker = new mapbox.Marker()
                     .setLngLat(e.lngLat)
-                    .addTo(map);
+                marker.addTo(map);
+                map.flyTo({
+                    center: e.lngLat,
+                    zoom: 10,
+                    duration: 2000,
+                    essential: true // this animation is considered essential with respect to prefers-reduced-motion
+                });
+                lat = e.lngLat.lat;
+                lng = e.lngLat.lng;
             });
             map.setFog({
                 'color': 'rgba(255, 255, 255, 0.3)',
@@ -61,19 +76,40 @@
         };
     });
 </script>
-
-<div bind:this={container}>
+<div class="columns">
+    <div class="column is-one-quarter">
+        <div id="map-interface" class="card">
+            <div class="card-header">
+                <h1 class="card-header-title">test</h1>
+            </div>
+            <div class="card-content">
+                <div class="content">
+                    <div id="geocoder" class="block"/>
+                    <div class="block">
+                        {lat}
+                        {lng}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="map" bind:this={container}>
     {#if map}
         <slot />
     {/if}
 </div>
 
 <style>
-	div {
+	#map {
         position: absolute; 
         top: 0; 
         bottom: 0; 
 		width: 100%;
 		height: 100%;
 	}
+
+    #map-interface {
+        z-index:1000;
+    }
 </style>
