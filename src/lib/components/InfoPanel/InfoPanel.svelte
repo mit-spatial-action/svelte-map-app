274983381698896b8c025303242fccef280567fe
Milan Chuttani <37783536+mchuttani18@users.noreply.github.com>
2023-11-15 14:55:32 -0500

    Added placeholder address and eviction data

--- src/lib/components/InfoPanel/InfoPanel.svelte
+++ src/lib/components/InfoPanel/InfoPanel.svelte
@@ -10,7 +10,12 @@
 </script>
 
 {#if loadState }
+    <div class="title has-text-dark is-size-1 has-text-centered">
+        {$selectedFeature[0].properties.landlord_name}
+    </div>
     <div class="subtitle mt-1 has-text-dark is-size-6 has-text-centered">
-        { $selectedFeature[0].properties.landlord_name }
+        Eviction Rank: { $selectedFeature[0].properties.eviction_rank } <br/>
+        Evictions: {$selectedFeature[0].properties.evictions} <br/>
+        Address: { $selectedFeature[0].properties.place_name} <br/>
     </div>
 {/if}
